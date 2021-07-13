ac3e4d33d6c6cb6fadae96f3cdb7ea0d55b956e8
ramsundark5 <ramsundark5@gmail.com>
2021-07-13 16:31:35 -0400

    added codepush feature (#9)
    * fixed credit updata and delete issues
    
    * ignore signing keys
    
    * added icons
    
    * updated welcome page
    
    * Codepush implemented (#8)
    
    * codepush initial commit
    
    * fixed the icons and codepush automation
    
    * limit codepush to only android
    
    Co-authored-by: ram.kuppusamy <ram.kuppusamy@navihealth.com>
    
    Co-authored-by: ram.kuppusamy <ram.kuppusamy@navihealth.com>

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -25,10 +25,7 @@ const Login: React.FC = () => {
         queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
         signInFlow: 'popup',
         signInSuccessUrl: '', // Specifying sign in success url can cause double redirect since we are also managing redirect in react-router with local state.
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        ],
+        signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
         // Enable one-tap sign-in.
         credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
         callbacks: {
@@ -54,14 +51,17 @@ const Login: React.FC = () => {
                 </IonRow>
                 <IonRow>
                     <IonCol className='text-center'>
-                        <IonText className='title'>Welcome to I love to care</IonText>
+                        <IonText className='title'>
+                            Welcome to i Love To Care (India) Charitable Trust ™
+                        </IonText>
                     </IonCol>
                 </IonRow>
                 <IonRow>
                     <IonCol className='text-center'>
                         <IonText className='text-center'>
-                            Volunteer organization to promote, support and enhance social
-                            responsibilities and charitable works.
+                            Volunteer Organization to promote, support and enhance social
+                            responsibilities and charitable works and works through implementation
+                            of ALL 17 United Nations Sustainable Development Goals throughout India.
                         </IonText>
                     </IonCol>
                 </IonRow>
