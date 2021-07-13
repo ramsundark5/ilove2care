55af2b8a98766f0d1c92b3e033b927082df70320
ram.kuppusamy <ram.kuppusamy@navihealth.com>
2021-07-13 16:29:26 -0400

    limit codepush to only android

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { setupConfig } from '@ionic/react'
+import { isPlatform, setupConfig } from '@ionic/react'
 import { codePush, InstallMode } from 'capacitor-codepush'
 
 import App from './App'
@@ -15,31 +15,33 @@ import RootStore from './stores'
 import './firebaseui-styling.global.css'
 
 // run codepush sync at start
-codePush
-    .sync({
-        updateDialog: true,
-        installMode: InstallMode.IMMEDIATE,
-        deploymentKey: process.env.REACT_APP_ANDROID_DEPLOYMENT_KEY,
-    })
-    .then((status) => {
-        switch (status) {
-            case 0:
-                log.info('codepush package upto date')
-                break
-            case 5:
-                log.info('checking codepush package update')
-                break
-            case 3:
-                log.error('Error downloading codepush package')
-                break
-            default:
-                log.info('codepush check complete')
-                break
-        }
-    })
-    .catch((err) => {
-        log.error(`error connecting to codepush ${err}`)
-    })
+if (isPlatform('android')) {
+    codePush
+        .sync({
+            updateDialog: true,
+            installMode: InstallMode.IMMEDIATE,
+            deploymentKey: process.env.REACT_APP_ANDROID_DEPLOYMENT_KEY,
+        })
+        .then((status) => {
+            switch (status) {
+                case 0:
+                    log.info('codepush package upto date')
+                    break
+                case 5:
+                    log.info('checking codepush package update')
+                    break
+                case 3:
+                    log.error('Error downloading codepush package')
+                    break
+                default:
+                    log.info('codepush check complete')
+                    break
+            }
+        })
+        .catch((err) => {
+            log.error(`error connecting to codepush ${err}`)
+        })
+}
 
 const rootStore = new RootStore()
 rootStore.init()
